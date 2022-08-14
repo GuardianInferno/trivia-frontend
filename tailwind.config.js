49a67aaed1cdeecd3e9d6f772076b4c1acad5730
Hashir <hashirabbasi112@gmail.com>
2022-08-13 21:42:19 -0400

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx}"],
+  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
   theme: {
     extend: {},
   },
